3bdc6960af96fc39802618de97fd2cd23111cab9
Rohan Poudel <nsrapoudel@gmail.com>
2022-07-27 11:40:12 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TopBar from "./components/topbar/TopBar"
+
+const App = () => {
+  return (
+    <div className="app">
+      <TopBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,11 @@
+import './topbar.scss'
+
+const TopBar = () => {
+  return (
+    <div className="top">
+      topbar
+    </div>
+  )
+}
+
+export default TopBar
